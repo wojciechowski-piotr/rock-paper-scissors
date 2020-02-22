87e8291c89f0da1621d5247048ff6281fc1d66ec
wojciechowski-piotr <pedro9493@gmail.com>
2020-02-22 10:46:41 +0100

    game.js - Move the startGame function to outside the Game class.

--- src/js/game.js
+++ src/js/game.js
@@ -3,25 +3,23 @@ import players from './players-list';
 
 class Game {
     constructor(pointLimit, rounds) {
-
         this.pointLimit = pointLimit;
         this.rounds = rounds;
         this.p1score = 0;
         this.p2score = 0;
     }
-    startGame() {
+}
 
-        weapons.forEach(el => el.createWeapon());
+Game.prototype.startGame = function() {
+    weapons.forEach(el => el.createWeapon());
         players[0].chooseWeapon(compareWeapons);
         
         function compareWeapons() {
-            
             const p1Weapon = document.querySelector('.player1__weapon-value').textContent;
             const p2Weapon = document.querySelector('.player2__weapon-value').textContent;
 
             const p1WeaponId = weapons.findIndex(el => el.name === p1Weapon);
             const p2WeaponId = weapons.findIndex(el => el.name === p2Weapon);
-
             
             if (p1Weapon === weapons[p2WeaponId].beat) {
                 console.log('P2 win');
@@ -30,8 +28,7 @@ class Game {
             } else if (p1Weapon === p2Weapon) {
                 console.log('Draw');
             }
-        }
-    }
-}
+        }    
+};
 
 export default Game;
